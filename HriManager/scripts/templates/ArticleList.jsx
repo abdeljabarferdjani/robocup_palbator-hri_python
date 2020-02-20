036299883beae0fd50bb45a5ed9266d1e95f6be2
darkpotatoe <thomas.cure@cpe.fr>
2020-02-20 10:18:22 +0100

    Updating branch HriToVoice

--- HriManager/scripts/templates/ArticleList.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import Article from './Article';export default class ArticleList extends Component {
-    render() {
-       return (
-          <div className='ui unstackable items'>
-             <Article />
-          </div>)
-    }
-}
--- arg_fetcher.py
+++
@@ -1,8 +0,0 @@
-from tools import logger
-
-def get_argument(args, arg_name):
-    try:
-        return args[arg_name]
-    except (KeyError, IndexError):
-        logger.log("Key not found: " + str(arg_name), "Arg Fetcher", logger.WARNING)
-        return None
